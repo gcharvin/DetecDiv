7606c70f0f9a8d40274390306334a4aebd4c4a01
Gilles Charvin <gilles.charvin@gmail.com>
2020-05-29 09:40:41 +0200

    adding new classification method for pedigree analysis

---
+++ classification/classifyPedigreeNetFun.m
@@ -0,0 +1,187 @@
+function roiout=classifyImageLSTMNetFun(roiobj,classif,classifier)
+
+%load([path '/netCNN.mat']); % load the googlenet to get the input size of image
+
+% now load and read video
+fprintf('Load videos...\n');
+
+%inputSize = netCNN.Layers(1).InputSize(1:2);
+
+%inputSize=[size(roiobj.image,1) size(roiobj.image,2)];
+inputSize = classifier.Layers(140).InputSize(1:2);
+
+%return;
+% x y size of the input movie (140th layer)
+%for i=id
+%fprintf(['Processing video:' num2str(i) '...\n']);
+%load([mov.path '/labeled_video_' mov.trap(i).id '.mat']); % loads deep, vid, lab (categories of labels)
+
+if numel(roiobj.image)==0
+    roiobj.load;
+end
+
+pix=find(roiobj.channelid==classif.channel(1)); % find channels corresponding to trained data
+im=roiobj.image(:,:,pix,:); 
+
+disp('Formatting video before classification....');
+%size(im)
+
+if numel(pix)==1
+    % 'ok'
+    totphc=im;
+    meanphc=0.5*double(mean(totphc(:)));
+    maxphc=double(meanphc+0.7*(max(totphc(:))-meanphc));
+end
+
+vid=uint8(zeros(size(im,1),size(im,2),3,size(im,4)));
+
+for j=1:size(im,4)
+    tmp=im(:,:,:,j);
+    
+    if numel(pix)==1
+        
+        tmp = double(imadjust(tmp,[meanphc/65535 maxphc/65535],[0 1]))/65535;
+        tmp=repmat(tmp,[1 1 3]);
+        
+    end
+    
+    vid(:,:,:,j)=uint8(256*tmp);
+    
+end
+
+%inputSize
+%size(vid)
+video = centerCrop(vid,inputSize);
+
+%size(video)
+%aa=classifier.Layers
+
+% nframes=inputSize(1);
+% narr=floor(size(im,4)/nframes);
+% nrem=mod(size(im,4),nframes);
+% 
+% if nrem>0
+%     narr=narr+1;
+% end
+% 
+% videoout={};
+% 
+% %if size(im,4)>nframes
+% for i=1:narr
+%     if i==narr
+%         ende=(i-1)*nframes+nrem;
+%     else
+%         ende=i*nframes ;
+%     end
+%    videoout{i}=video(:,:,:,(i-1)*nframes+1:ende);
+% end
+% %else
+% %   videoout{1}=video;
+% %end
+% 
+% %size(videoout)
+% %size(videoout{1})
+% %size(videoout{3})
+
+disp('Starting video classification...');
+
+test=predict(classifier,video);
+
+[~, idx] = max(test,[],2);
+labels = classifier.Layers(end).Classes;
+label = labels(idx);
+
+%label = classify(classifier,video);
+
+%label=[];
+
+% lab=[];
+% %if size(im,4)>nframes
+% for i=1:narr
+%    lab = [lab label{i}];
+% end
+%else
+   %label=label{1}; 
+%end
+
+%label=lab(1:size(im,4));
+
+results=roiobj.results;
+    results.(classif.strid)=[];
+    results.(classif.strid).id=zeros(1,size(im,4));
+    results.(classif.strid).labels=label';
+    results.(classif.strid).classes=classif.classes;
+     
+  %  roiobj.results=results;
+    
+    for i=1:numel(classif.classes)
+        
+   pix=label==classif.classes{i};
+   results.(classif.strid).id(pix)=i;
+    
+    end
+
+roiobj.results=results; 
+
+roiout=roiobj;
+%roiobj.clear;    
+
+% results.id=roiobj.id;
+% results.path=roiobj.path;
+% results.parent=roiobj.parent;
+% 
+% % stores results locally during classification
+% 
+% if exist([classif.path '/' classif.strid '_results.mat']) % this filles needs to be removed when classification starts ? 
+%     load([classif.path '/' classif.strid '_results.mat']) % load res variable
+%     n=length(res);
+%     res(n+1)={results};
+% else
+%     
+%    res={results}; 
+% end
+% save([classif.path '/' classif.strid '_results.mat'],'res');
+
+
+
+%
+% pix=label=='largebudded';
+% mov.trap(i).div.deepLSTM(pix)=2;
+%
+% pix=label=='smallbudded';
+% mov.trap(i).div.deepLSTM(pix)=1;
+%
+% pix=label=='unbudded';
+% mov.trap(i).div.deepLSTM(pix)=0;
+
+
+%mov.trap(i).div.deepLSTM=YPred;
+
+
+
+function videoResized = centerCrop(video,inputSize)
+
+sz = size(video);
+
+if sz(1) < sz(2)
+    % Video is landscape
+    idx = floor((sz(2) - sz(1))/2);
+    video(:,1:(idx-1),:,:) = [];
+    video(:,(sz(1)+1):end,:,:) = [];
+    
+elseif sz(2) < sz(1)
+    % Video is portrait
+    idx = floor((sz(1) - sz(2))/2);
+    video(1:(idx-1),:,:,:) = [];
+    video((sz(2)+1):end,:,:,:) = [];
+end
+
+videoResized = imresize(video,inputSize(1:2));
+
+
+%analyzeNetwork(lgraph)
+
+
+
+
+%etc ...
---
+++ classification/trainPedigreeNetFun.m
@@ -0,0 +1,237 @@
+function trainPedigreeNetFun(path,name)
+
+% first train googlenet network based on specific images
+
+% if nargin<4
+% load([mov.path '/netCNN.mat']); % loads pretrained googlenet, that has been retrained on
+% %specific images (transfer earning)
+% end
+
+
+load([ path '/options.mat']); % loading options for training --> imageclassifier, cactivations, lstm_training, assemblenet, validation
+
+%%% training google net classifier independtly
+
+if strcmp(imageclassifier,'y')
+    feval('trainImageGoogleNetFun',path,'netCNN'); % trainImageGoogle net first and saves it as netCNN.mat in the LSTM dir
+    % corresponding variable is 'classifier'
+end
+
+  load([ path '/netCNN.mat']);
+  netCNN=classifier;
+    
+  
+%%%
+
+%%% Computer activations from the google net network based on the training
+%%% set 
+
+% load and format data as sequences of vectors using spepcific network
+% layer
+
+inputSize = netCNN.Layers(1).InputSize(1:2);
+layerName = "pool5-7x7_s1";
+
+% load all the files in the timeseries
+fol= [path '/trainingdataset/timeseries'];
+list=dir([fol '/*.mat']);
+
+% for i=1:numel(list)
+%     
+% end
+
+%%%% HERE need to gather files in correct folders
+numFiles = numel(list);
+
+sequences = cell(numFiles,1);
+labels = cell(numFiles,1);
+
+tempFile = [path '/' name '_googlenet_activations.mat']; % loads vid, lab, deep variables
+% label in an array of categorical labels, vid is a video file of uint8
+
+%cc=1;
+% cactivations='n'; %warning !
+% list
+
+if ~strcmp(cactivations,'y') %exist(tempFile,'file')
+    load(tempFile,"sequences","labels")
+else
+    cc=1;
+    for i=1:numel(list)
+        
+        fprintf(['Processing movie ' num2str(i) '...']);
+        
+        load([list(i).folder '/' list(i).name]); % loads deep, vid, lab (categories of labels)
+        
+        video = centerCrop(vid,inputSize);
+        
+        sequences{cc,1} = activations(netCNN,video,layerName,'OutputAs','columns');
+        labels{cc,1}= lab;
+        cc=cc+1;
+        fprintf('\n');
+    end
+    
+    save(tempFile,"sequences","labels","-v7.3");
+    fprintf('\n');
+end
+
+%return; 
+
+if strcmp(lstmtraining,'y') % training of LSTM network
+    
+    disp('Preparing LSTM network ...');
+% prepare training data : 90% in training and 10% used for validation
+
+numObservations = numel(sequences);
+idx = randperm(numObservations);
+N = floor(0.9 * numObservations); % 0.9 replace
+
+idxTrain = idx(1:N);
+%idxTrain=1; % warning
+
+sequencesTrain = sequences(idxTrain);
+labelsTrain = labels(idxTrain);
+
+idxValidation = idx(N+1:end);
+%idxValidation = 1; %warning
+
+sequencesValidation = sequences(idxValidation);
+labelsValidation = labels(idxValidation);
+
+% create LSTM network
+
+numFeatures = size(sequencesTrain{1},1);
+numClasses = numel(categories(labelsTrain{1}));
+
+%return;
+layers = [
+    sequenceInputLayer(numFeatures,'Name','sequence')
+    bilstmLayer(2000,'OutputMode','sequence','Name','bilstm')
+   % lstmLayer(200,'OutputMode','sequence','Name','bilstm')
+    dropoutLayer(0.5,'Name','drop')
+    fullyConnectedLayer(numClasses,'Name','fc')
+    softmaxLayer('Name','softmax')
+    classificationLayer('Name','classification')];
+
+% specifiy training options
+
+miniBatchSize = 16;
+numObservations = numel(sequencesTrain);
+numIterationsPerEpoch = max(1,floor(numObservations / miniBatchSize));
+
+options = trainingOptions('adam', ...
+    'MiniBatchSize',miniBatchSize, ...
+    'InitialLearnRate',1e-4, ...
+    'GradientThreshold',2, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',{sequencesValidation,labelsValidation}, ...
+    'ValidationFrequency',numIterationsPerEpoch, ...
+    'Plots','training-progress', ...
+    'Verbose',false);
+
+%options.SequenceLength
+%options.SequencePaddingValue
+%return;
+% train network
+ disp('Training LSTM network ...');
+[netLSTM,info] = trainNetwork(sequencesTrain,labelsTrain,layers,options);
+
+save([path '/netLSTM.mat'],'netLSTM','info');
+ disp('Training LSTM network is done and saved ...');
+ 
+else
+ load([path '/netLSTM.mat']); 
+  disp('Loading LSTM network ...');
+end
+
+
+%%% assemble the full network
+%%%
+
+if strcmp(assemblenet,'y')
+%if numel(netFull)==0
+%load([mov.path '/netLSTM.mat']);
+
+
+% remove non necessary layers
+fprintf(' remove output layers from CNN net\n');
+
+cnnLayers = layerGraph(netCNN);
+%layerNames = ["data" "pool5-drop_7x7_s1" "loss3-classifier" "prob" "output"];
+layerNames = ["data" "pool5-drop_7x7_s1" "new_fc" "prob" "new_classoutput"];
+cnnLayers = removeLayers(cnnLayers,layerNames);
+
+% create layers to adjust to CNN network layers
+fprintf(' create layers to adjust to CNN network layers\n');
+
+inputSize = netCNN.Layers(1).InputSize(1:2);
+averageImage = netCNN.Layers(1).AverageImage;
+
+inputLayer = sequenceInputLayer([inputSize 3], ...
+    'Normalization','zerocenter', ...
+    'Mean',averageImage, ...
+    'Name','input');
+
+% add the sequence input layer to the layer graph
+layers = [
+    inputLayer
+    sequenceFoldingLayer('Name','fold')];
+
+lgraph = addLayers(cnnLayers,layers);
+lgraph = connectLayers(lgraph,"fold/out","conv1-7x7_s2");
+
+% create lstm network and remove first layer (sequence)
+fprintf(' create LSTM network\n');
+
+lstmLayers = netLSTM.Layers;
+lstmLayers(1) = [];
+
+layers = [
+    sequenceUnfoldingLayer('Name','unfold')
+    flattenLayer('Name','flatten')
+    lstmLayers];
+
+lgraph = addLayers(lgraph,layers);
+lgraph = connectLayers(lgraph,"pool5-7x7_s1","unfold/in");
+lgraph = connectLayers(lgraph,"fold/miniBatchSize","unfold/miniBatchSize");
+
+%analyzeNetwork(lgraph) 
+
+fprintf('Assemble full network\n');
+
+classifier = assembleNetwork(lgraph);
+save([path '/' name '.mat'],'classifier');
+
+
+fprintf('Full network is assembled !\n');
+
+else
+   load( [path '/' name '.mat']); % loading the fully assembled network
+end
+
+
+
+
+%end
+
+
+
+
+function videoResized = centerCrop(video,inputSize)
+
+sz = size(video);
+
+if sz(1) < sz(2)
+    % Video is landscape
+    idx = floor((sz(2) - sz(1))/2);
+    video(:,1:(idx-1),:,:) = [];
+    video(:,(sz(1)+1):end,:,:) = [];
+    
+elseif sz(2) < sz(1)
+    % Video is portrait
+    idx = floor((sz(1) - sz(2))/2);
+    video(1:(idx-1),:,:,:) = [];
+    video((sz(2)+1):end,:,:,:) = [];
+end
+
+videoResized = imresize(video,inputSize(1:2));
