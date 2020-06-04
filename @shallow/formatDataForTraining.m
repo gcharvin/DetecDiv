51f19f533caf81ac77209aafacac409e0d51f39a
Gilles Charvin <gilles.charvin@gmail.com>
2020-06-04 19:22:42 +0200

    try fine tune

--- @shallow/formatDataForTraining.m
+++ @shallow/formatDataForTraining.m
@@ -503,12 +503,12 @@ parfor i=rois
                       bwtot= bw1 | bw2; % objects in close proximity
                       bwtot=imdilate(bwtot,strel('Disk',3));
                       bw= bw | bwtot; % mask with interestings pairs
-                      %bw=imdilate(bw,strel('Disk',10)); % dilate the whole mask a bit
+                      bw=imdilate(bw,strel('Disk',20)); % dilate the whole mask a bit
                       
                       else % before bud emerges, takes only the mother into account
                       
                         bw=  im2(:,:,1,ll)==neighborsList(mm);
-                       % bw=imdilate(bw,strel('Disk',10)); % dilate the whole mask a bit
+                        bw=imdilate(bw,strel('Disk',20)); % dilate the whole mask a bit
                       end
                     
                       imtmp=uint16(zeros(size(im,1),size(im,2)));
