6b86906fedb98fc20a179bba8eebe6e8762394c3
Gilles Charvin <gilles.charvin@gmail.com>
2021-01-27 09:59:40 +0100

    added new layer - bugfix

---
+++ @weightedClassificationLayer/weightedClassificationLayer.m
@@ -0,0 +1,47 @@
+classdef weightedClassificationLayer < nnet.layer.ClassificationLayer
+   properties
+        % Vector of weights corresponding to the classes in the training
+        % data
+        ClassWeights
+    end
+    
+    
+    methods
+        function layer = weightedClassificationLayer(classWeights, name)
+            % layer = weightedClassificationLayer(classWeights) creates a
+            % weighted cross entropy loss layer. classWeights is a row
+            % vector of weights corresponding to the classes in the order
+            % that they appear in the training data.
+            % 
+            % layer = weightedClassificationLayer(classWeights, name)
+            % additionally specifies the layer name. 
+
+            % Set class weights
+            layer.ClassWeights = classWeights;
+
+            % Set layer name
+            if nargin == 2
+                layer.Name = name;
+            end
+
+            % Set layer description
+            layer.Description = 'Weighted cross entropy';
+        end
+        
+        function loss = forwardLoss(layer, Y, T)
+            % loss = forwardLoss(layer, Y, T) returns the weighted cross
+            % entropy loss between the predictions Y and the training
+            % targets T.
+
+            N = size(Y,4);
+            Y = squeeze(Y);
+            T = squeeze(T);
+            W = layer.ClassWeights;
+    
+            loss = -sum(W*(T.*log(Y)))/N;
+        end
+
+     end 
+    
+    
+end
