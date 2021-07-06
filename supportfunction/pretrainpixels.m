8b119bbf335ebbccd2253a5e2d6dd66ebf54af42
TAspert <theo.aspert@gmail.Com>
2021-07-06 11:18:58 +0200

    Added fluo pretraining by thresholding

--- supportfunction/pretrainpixels.m
+++ supportfunction/pretrainpixels.m
@@ -1,27 +1,46 @@
-function pretrainobjects(obj,frames,option)
-% this function assigns pixel values using standard cell segmentation 
+function preTrainPixels(obj,varargin)
+% this function assigns pixel values using standard cell segmentation
 
-% obj.train : train is an uint8 rgb image W x H x 3 x Frames : red and green channel used 
+% obj.train : train is an uint8 rgb image W x H x 3 x Frames : red and green channel used
+
+type='fluo';
+channel=2;
+frames=1;
+threshold=1;
+
+for i=1:numel(varargin)
+    %Type
+    if strcmp(varargin{i},'Type')
+        type=varargin{i+1};
+    end
+    %frames
+    if strcmp(varargin{i},'Frames')
+        frames=varargin{i+1};
+    end
+    if strcmp(varargin{i},'Channel')
+        channel=varargin{i+1};
+    end    
+    if strcmp(varargin{i},'Threshold')
+        threshold=varargin{i+1};
+    end
+end
 
 
 %obj.view(obj.frame)
 for i=frames
-fprintf('.');
-
-img= obj.gfp(:,:,i,obj.phasechannel);
-
-BW=segmentPhaseContrast(img);
-
-%figure, imshow(BW,[]);
-
-obj.train(:,:,1,i)=255*~BW;
-obj.train(:,:,2,i)=255*BW;
-
-
-%figure, imshow(obj.train(:,:,2,obj.frame))
-
-%obj.view(obj.frame)
-
+    fprintf('.');
+    
+    img= obj.image(:,:,channel,i);
+    
+    if strcmp(type,'fluo')
+        BW=segmentFluo(img,threshold);
+    else
+        BW=segmentPhaseContrast(img);
+    end
+    
+    %to adapt for
+    %robustness!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    obj.image(:,:,channel+1,i)=255*BW;
 end
 
 fprintf('\n');
@@ -33,15 +52,15 @@ param=struct('channel',1,'minSize',100,'maxSize',10000,'thresh',0.25,'display',0
 img2=img;
 
 if param.display==1
-scr=get(0,'ScreenSize');   
-figure('Color','w','Position',[1 scr(3)-500 scr(3) 500]); p=panel; p.de.margin=0; p.pack('h',1); ccc=1; p(ccc).select(); 
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-imshow(img,[]);
+    scr=get(0,'ScreenSize');
+    figure('Color','w','Position',[1 scr(3)-500 scr(3) 500]); p=panel; p.de.margin=0; p.pack('h',1); ccc=1; p(ccc).select();
+    p(ccc).marginleft=0;
+    p(ccc).marginright=0;
+    imshow(img,[]);
 end
-  
 
-img = rangefilt(img); 
+
+img = rangefilt(img);
 %[img ~]=imgradient(img);
 %figure, imshow(img,[]);
 
@@ -52,17 +71,17 @@ img = rangefilt(img);
 %figure, imshow(img,[]);
 
 if param.display==1
-p.pack('h',1); ccc=ccc+1; p(ccc).select();
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-imshow((img),[]);
-  end
+    p.pack('h',1); ccc=ccc+1; p(ccc).select();
+    p(ccc).marginleft=0;
+    p(ccc).marginright=0;
+    imshow((img),[]);
+end
 %returns thresh containing N threshold values using Otsu's method
 
 % if param.thresh==0
 % level = graythresh(img);
 % else
-% level=param.thresh;    
+% level=param.thresh;
 % end
 
 %class(img2)
@@ -76,129 +95,129 @@ BW2=imbinarize(uint16(img2),T);
 
 
 if param.display==1
-p.pack('h',1); ccc=ccc+1; p(ccc).select();
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-imshow(BW2,[]);
-  end
+    p.pack('h',1); ccc=ccc+1; p(ccc).select();
+    p(ccc).marginleft=0;
+    p(ccc).marginright=0;
+    imshow(BW2,[]);
+end
 
 
-for j=1:1 % currently, only one loop is necessary 
-   % l=0.005-0.001*(j); % for log filter
+for j=1:1 % currently, only one loop is necessary
+    % l=0.005-0.001*(j); % for log filter
     
-  if param.thresh~=0
-     l=param.thresh-0.02*(j-1);
-     BW=edge(img,'canny',l); % try other edge detection filters ? 
-  else
-     BW=edge(img,'canny');    
-  end
-  
-  
-  if param.display==1
-p.pack('h',1); ccc=ccc+1; p(ccc).select();
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-
-imshow(BW,[]);
-  end
-  
-  
-%BW = im2bw(img,level);
-%BW = BW | BW2;
-BW2 = bwareaopen(BW2, 10);
-
-BW = bwareaopen(BW, 10);
-
-%BW=fgm;
-%BW = im2bw(img,l);
-%BW=BW3;
-
-
-%BW=imopen(BW,strel('disk',2));
-
-%if param.display
-%figure,imshow(BW,[]);
-%end
-
-if ~ischar(param.mask)
-    BW=BW | param.mask;
+    if param.thresh~=0
+        l=param.thresh-0.02*(j-1);
+        BW=edge(img,'canny',l); % try other edge detection filters ?
+    else
+        BW=edge(img,'canny');
+    end
     
-if param.display==1
-p.pack('h',1); ccc=ccc+1; p(ccc).select();
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-imshow(BW,[]);
-  end
+    
+    if param.display==1
+        p.pack('h',1); ccc=ccc+1; p(ccc).select();
+        p(ccc).marginleft=0;
+        p(ccc).marginright=0;
+        
+        imshow(BW,[]);
+    end
+    
+    
+    %BW = im2bw(img,level);
+    %BW = BW | BW2;
+    BW2 = bwareaopen(BW2, 10);
+    
+    BW = bwareaopen(BW, 10);
+    
+    %BW=fgm;
+    %BW = im2bw(img,l);
+    %BW=BW3;
+    
+    
+    %BW=imopen(BW,strel('disk',2));
+    
+    %if param.display
+    %figure,imshow(BW,[]);
+    %end
+    
+    if ~ischar(param.mask)
+        BW=BW | param.mask;
+        
+        if param.display==1
+            p.pack('h',1); ccc=ccc+1; p(ccc).select();
+            p(ccc).marginleft=0;
+            p(ccc).marginright=0;
+            imshow(BW,[]);
+        end
+    end
+    
+    
+    
+    imdist=bwdist(BW2);
+    imdist = imclose(imdist, strel('disk',2));
+    imdist = imhmax(imdist,2);
+    
+    if param.display==1
+        p.pack('h',1); ccc=ccc+1; p(ccc).select();
+        p(ccc).marginleft=0;
+        p(ccc).marginright=0;
+        imshow(imdist,[0 30]); colormap(jet)
+    end
+    
+    sous=BW2- imdist;
+    
+    if ~ischar(param.mask)
+        BW2=BW2 | param.mask;
+    end
+    
+    
+    %BW=logical(zeros(size(BW)));
+    %BW(imdist<60)=1;
+    
+    %figure, imshow(BW,[]);
+    
+    labels = double(watershed(sous,8)).* ~BW2;% .* BW % .* param.mask; % watershed
+    warning off all
+    tmp = imopen(labels > 0, strel('disk', 4));
+    warning on all
+    tmp = bwareaopen(tmp, 50);
+    
+    newlabels = labels .* tmp; % remove small features
+    newlabels = bwlabel(newlabels>0);
+    
+    warning off all
+    %figure, imshow(newlabels,[]);
+    warning on all
+    
+    
+    % if j>1
+    %     %figure, imshow(oldlabels,[]);
+    %     %figure, imshow(newlabels,[]);
+    %
+    %     M=buildMatrix(oldlabels,newlabels);
+    %    [Matching,Cost] = Hungarian(M);
+    %    %[row,col] = find(Matching);
+    %    newlabels=updateLabels(oldlabels,newlabels,Matching);
+    %
+    %     %figure, imshow(newlabels,[]);
+    % end
+    
+    oldlabels = newlabels;
 end
 
-
-
-imdist=bwdist(BW2);
-imdist = imclose(imdist, strel('disk',2));
-imdist = imhmax(imdist,2);
-
 if param.display==1
-p.pack('h',1); ccc=ccc+1; p(ccc).select();
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-imshow(imdist,[0 30]); colormap(jet)
-  end
-
-sous=BW2- imdist;
-
-if ~ischar(param.mask)
-   BW2=BW2 | param.mask; 
+    p.pack('h',1); ccc=ccc+1; p(ccc).select();
+    p(ccc).marginleft=0;
+    p(ccc).marginright=0;
+    imshow(newlabels,[]);
 end
-
-
-%BW=logical(zeros(size(BW)));
-%BW(imdist<60)=1;
-
-%figure, imshow(BW,[]);
-
-labels = double(watershed(sous,8)).* ~BW2;% .* BW % .* param.mask; % watershed
-warning off all
-tmp = imopen(labels > 0, strel('disk', 4));
-warning on all
-tmp = bwareaopen(tmp, 50);
-
-newlabels = labels .* tmp; % remove small features
-newlabels = bwlabel(newlabels>0);
-
-warning off all
-%figure, imshow(newlabels,[]);
-warning on all
-
-
-% if j>1
-%     %figure, imshow(oldlabels,[]);
-%     %figure, imshow(newlabels,[]);
-%     
-%     M=buildMatrix(oldlabels,newlabels);
-%    [Matching,Cost] = Hungarian(M);
-%    %[row,col] = find(Matching);
-%    newlabels=updateLabels(oldlabels,newlabels,Matching);
-%    
-%     %figure, imshow(newlabels,[]);
-% end
-
-oldlabels = newlabels;
-end
-
-if param.display==1
-p.pack('h',1); ccc=ccc+1; p(ccc).select();
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-imshow(newlabels,[]);
-  end
 
 % if sca~=1
 %  img=imresize(img,sca);
 % end
- 
- %newlabels=imresize(newlabels,2);
- 
- %newlabels=uint16(newlabels);
+
+%newlabels=imresize(newlabels,2);
+
+%newlabels=uint16(newlabels);
 
 
 
@@ -214,57 +233,57 @@ newlabels2=uint8(zeros(size(newlabels)));
 cc=1;
 
 for i=1:numel(stat)
-   tmp=newlabels==i;
-   %if i==59
-   %a=stat(i).Area
-   %end
-   
-   
-   bb=stat(i).BoundingBox;
-   
-   if bb(1)<1
-       %newlabels(tmp)=0;
-       continue
-   end
-   if bb(2)<1
-       %newlabels(tmp)=0;
-       continue
-   end
-   if bb(2)+bb(4)>size(newlabels,2)
-       %newlabels(tmp)=0;
-       continue
-   end
-   if bb(1)+bb(3)>size(newlabels,2)
-       %newlabels(tmp)=0;
-       continue
-   end
-   
-   if stat(i).Area <param.minSize || stat(i).Area >param.maxSize %|| stat(i).Eccentricity>0.9
-      
-   %newlabels(tmp)=0;
-   continue
-   else
-   %contours= bwboundaries(tmp);
-   %contour = contours{1};
-   %[xnew, ynew]=phy_changePointNumber(contour(:, 2),contour(:, 1),npoints);
-   
-   %if min(sca*xnew)>1 && min(sca*ynew)>1 && max(sca*xnew)<size(img,2) && max(sca*ynew)<size(img,1)
-   %phy_Objects(cc) = phy_Object(cc, sca*(xnew+1), sca*(ynew+1),0,0,mean(sca*(xnew+1)),mean(sca*(ynew+1)),0);
-   
-  
-   %phy_Objects(cc).fluoMean(1)=mean(stat(i).PixelValues);
-   %phy_Objects(cc).fluoVar(1)=std(double(stat(i).PixelValues));
-   
-   %if param.display
-   %    line( phy_Objects(cc).x,phy_Objects(cc).y,'Color','r','LineWidth',2);
-      % text(phy_Objects(cc).ox,phy_Objects(cc).oy,num2str(phy_Objects(cc).n),'Color','r','FontSize',24);
-   %end
-   
-   %cc=cc+1;
-   %end
-   end
-   newlabels2(tmp)=cc;
-   cc=cc+1;
+    tmp=newlabels==i;
+    %if i==59
+    %a=stat(i).Area
+    %end
+    
+    
+    bb=stat(i).BoundingBox;
+    
+    if bb(1)<1
+        %newlabels(tmp)=0;
+        continue
+    end
+    if bb(2)<1
+        %newlabels(tmp)=0;
+        continue
+    end
+    if bb(2)+bb(4)>size(newlabels,2)
+        %newlabels(tmp)=0;
+        continue
+    end
+    if bb(1)+bb(3)>size(newlabels,2)
+        %newlabels(tmp)=0;
+        continue
+    end
+    
+    if stat(i).Area <param.minSize || stat(i).Area >param.maxSize %|| stat(i).Eccentricity>0.9
+        
+        %newlabels(tmp)=0;
+        continue
+    else
+        %contours= bwboundaries(tmp);
+        %contour = contours{1};
+        %[xnew, ynew]=phy_changePointNumber(contour(:, 2),contour(:, 1),npoints);
+        
+        %if min(sca*xnew)>1 && min(sca*ynew)>1 && max(sca*xnew)<size(img,2) && max(sca*ynew)<size(img,1)
+        %phy_Objects(cc) = phy_Object(cc, sca*(xnew+1), sca*(ynew+1),0,0,mean(sca*(xnew+1)),mean(sca*(ynew+1)),0);
+        
+        
+        %phy_Objects(cc).fluoMean(1)=mean(stat(i).PixelValues);
+        %phy_Objects(cc).fluoVar(1)=std(double(stat(i).PixelValues));
+        
+        %if param.display
+        %    line( phy_Objects(cc).x,phy_Objects(cc).y,'Color','r','LineWidth',2);
+        % text(phy_Objects(cc).ox,phy_Objects(cc).oy,num2str(phy_Objects(cc).n),'Color','r','FontSize',24);
+        %end
+        
+        %cc=cc+1;
+        %end
+    end
+    newlabels2(tmp)=cc;
+    cc=cc+1;
 end
 
 stat = regionprops(newlabels2, 'Area','Eccentricity','BoundingBox');
@@ -292,53 +311,32 @@ OK=1;
 BW=newlabels2>0;
 
 if param.display==1
-p.pack('h',1); ccc=ccc+1; p(ccc).select();
-p(ccc).marginleft=0;
-p(ccc).marginright=0;
-imshow(BW,[]);
+    p.pack('h',1); ccc=ccc+1; p(ccc).select();
+    p(ccc).marginleft=0;
+    p(ccc).marginright=0;
+    imshow(BW,[]);
 end
-  
+
 
 if param.display
-p.marginleft=0;
-p.marginright=0;
+    p.marginleft=0;
+    p.marginright=0;
+end
+
+function BW2=segmentFluo(img,threshold)
+img2=img;
+img2=im2double(img2);
+
+img2=img2-min(img2(:));
+img2=img2/max(img2(:));
+
+T=graythresh(img2);
+
+BW2=imbinarize(img2,T*1);
+BW2 = bwareaopen(BW2, 3);
+
+if mean(BW2(:))>0.3
+    BW2(:,:)=0;
 end
 
 
-% 
-% if nargin==1
-% frames=1:size(obj.traintrack,4);
-% end
-% 
-% for i=frames
-%     obj.traintrack(:,:,3,i)=uint8(zeros(size(obj.gfp,1),size(obj.gfp,2)));
-% end
-% 
-% if nargin==3
-%     return; % with this option, this remoces the training... 
-% end
-% 
-% for i=frames
-%     
-%     n=bwlabel(obj.traintrack(:,:,2,i)>0);
-%     p=regionprops(n,'Centroid');
-%     
-%     %p.Centroid
-%     
-%     if numel(p)==0
-%         continue
-%     end
-%     
-%     d=[];
-%     for j=1:numel(p)
-%        % j
-%         d(j)=sqrt((p(j).Centroid(1)-size(obj.gfp,2)/2)^2+(p(j).Centroid(2)-size(obj.gfp,1)/2)^2);
-%     end
-%     
-%    %d
-%   
-%     [dmin ix]=min(d);
-%     nc=n==ix;
-%     
-%     obj.traintrack(:,:,3,i)=255*uint8(nc);
-% end
