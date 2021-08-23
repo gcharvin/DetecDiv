4bac67af48bb18813c6f5ffa7306af0883d633d2
Gilles Charvin <gilles.charvin@gmail.com>
2021-08-23 22:18:26 +0200

    improved FOV / ROI inheritance with .log method  and .parent field

--- @fov/addROI.m
+++ @fov/addROI.m
@@ -5,8 +5,10 @@ function addROI(obj,roival,fovparentid)
 if numel(obj.roi)==0 | numel(obj.roi(1).id)==0 % currently no ROI defined
     obj.roi(1) = roi([obj.id '_' num2str(1)],roival);
     obj.roi(1).parent=fovparentid;
+    obj.roi(1).log(['ROI was created from FOV ' obj.id ' with path ' obj.srcpath{1}],'Creation');
 else
     tmp=numel(obj.roi);
     obj.roi(tmp+1) = roi([obj.id '_' num2str(tmp+1)],roival);
     obj.roi(tmp+1).parent=fovparentid;
+    obj.roi(tmp+1).log(['ROI was created from FOV ' obj.id ' with path ' obj.srcpath{1}],'Creation');
 end
