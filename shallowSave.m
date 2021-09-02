a27d01f75df54e88c1b6c0100d0496ec87fdf109
TAspert <theo.aspert@gmail.Com>
2021-09-02 22:10:56 +0200

    possibility to save only shallowObj

--- shallowSave.m
+++ shallowSave.m
@@ -1,51 +1,60 @@
-function shallowSave(shallowObj)
+function shallowSave(shallowObj,option)
 
 
 [path,file]=shallowObj.getPath;
 
 reverseStr='';
 cc=1;
+shallowObjOnly=0;
+
+if nargin==2
+    if strcmp(option,'shallowObj') % load only the results
+        shallowObjOnly=1;
+        disp(['Saving only shallowObj ' obj.id]);
+    end
+end
 
 fprintf('\n');
 
-for i=1:numel(shallowObj.fov)
-               for j=1:numel(shallowObj.fov(i).roi)
-                  % tic
-                  shallowObj.fov(i).roi(j).save;
-                  %toc
-                 % tic
-                  shallowObj.fov(i).roi(j).clear;
-                %  toc
-                  
-                    
-               end
-               
-               msg = sprintf('Writing ROIs for FOV %d / %d for FOV %s', cc,numel(shallowObj.fov)); %Don't forget this semicolon
-                    fprintf([reverseStr, msg]);
-                    reverseStr = repmat(sprintf('\b'), 1, length(msg));
-                    
-                    cc=cc+1;
-end
-
-% fprintf('\n');
-% reverseStr='';
-% cc=1;
-for i=1:numel(shallowObj.processing.classification)
+if shallowObjOnly==0
+    for i=1:numel(shallowObj.fov)
+        for j=1:numel(shallowObj.fov(i).roi)
+            % tic
+            shallowObj.fov(i).roi(j).save;
+            %toc
+            % tic
+            shallowObj.fov(i).roi(j).clear;
+            %  toc
+            
+            
+        end
+        
+        msg = sprintf('Writing ROIs for FOV %d / %d for FOV %s', cc,numel(shallowObj.fov)); %Don't forget this semicolon
+        fprintf([reverseStr, msg]);
+        reverseStr = repmat(sprintf('\b'), 1, length(msg));
+        
+        cc=cc+1;
+    end
     
-    
-    classiSave( shallowObj.processing.classification(i) );
-%                for j=1:numel(shallowObj.processing.classification(i).roi)
-%                   shallowObj.processing.classification(i).roi(j).save;
-%                   shallowObj.processing.classification(i).roi(j).clear;
-%                end
-%                
-%                 msg = sprintf('Writing ROIs for classification %d / %d for FOV %s', cc,numel(shallowObj.processing.classification)); %Don't forget this semicolon
-%                     fprintf([reverseStr, msg]);
-%                     reverseStr = repmat(sprintf('\b'), 1, length(msg));
-%                     
-%                     cc=cc+1;
+    % fprintf('\n');
+    % reverseStr='';
+    % cc=1;
+    for i=1:numel(shallowObj.processing.classification)
+        
+        
+        classiSave( shallowObj.processing.classification(i) );
+        %                for j=1:numel(shallowObj.processing.classification(i).roi)
+        %                   shallowObj.processing.classification(i).roi(j).save;
+        %                   shallowObj.processing.classification(i).roi(j).clear;
+        %                end
+        %
+        %                 msg = sprintf('Writing ROIs for classification %d / %d for FOV %s', cc,numel(shallowObj.processing.classification)); %Don't forget this semicolon
+        %                     fprintf([reverseStr, msg]);
+        %                     reverseStr = repmat(sprintf('\b'), 1, length(msg));
+        %
+        %                     cc=cc+1;
+    end
 end
-           
 
 fprintf('\n');
 
