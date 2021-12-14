cc8f8da3790770706dde3a610846844aed8b3328
Gilles Charvin <gilles.charvin@gmail.com>
2021-12-14 08:58:01 +0100

    removed unecessary files

--- deprecated/setcavity.m
+++
@@ -1,5 +0,0 @@
-function setcavity(obj)
-% set different polygons used to classify divisions
-
-obj.cavity.inputpoly=[]; % total cavity region, also used by fake nuclei to set nuclei size
-obj.cavity.topcavity=[]; % window at the top 
