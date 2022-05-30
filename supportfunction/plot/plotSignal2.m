1955df3da4e88f80c8b933370994f8c87cf2f91e
TAspert <theo.aspert@gmail.Com>
2022-05-30 20:32:17 +0200

    Update plotSignal2.m

--- supportfunction/plot/plotSignal2.m
+++ supportfunction/plot/plotSignal2.m
@@ -69,9 +69,9 @@ fluostrid='';
 %% load data if required
 if load==1
     for cond=conditions
-    for r=rois{cond}
-        rlsfile(r).load('results');
-    end
+        for r=rois{cond}
+            rlsfile(r).load('results');
+        end
     end
 end
 
@@ -93,116 +93,76 @@ if timeOrGen==0 %time
         error(['The roi ' rlsfile(1) 'has no signal. Extract it using extractSignal'])
     end
     
-elseif timeOrGen==1 %generations
-    
-    %     if RLSfile==0 %if input is rois array
-    %         % ask RLSclassi
-    %         if isfield(rls(1).results,'RLS')
-    %             liststrid=fields(rls(1).results.RLS); %full, cell, nucleus
-    %             str=[];
-    %         else
-    %             error(['The roi ' rls(1) 'has no RLS. Extract it using measureRLS3'])
-    %         end
-    %         for i=1:numel(liststrid)
-    %             str=[str num2str(i) ' - ' liststrid{i} ';'];
-    %         end
-    %         str=erase(str,'from_');
-    %
-    %         classiRLSid=input(['Which classi used for RLS extraction? (Default: 1)' str]);
-    %         if numel(classiRLSid)==0
-    %             classiRLSid=1;
-    %         end
-    %         classifRLSstrid=liststrid{classiRLSid};
-    %
-    %         if isfield(rls(1).results.RLS.(classifRLSstrid),'signal')
-    %             for r=rois
-    %                 obj(r,1)=rls(r).results.RLS.(classifRLSstrid).divSignal; %assign obj
-    %             end
-    %
-    %         else
-    %             error(['The roi ' rls(1) 'has no RLS.signal. Extract it using extractSignal followed by measureRLS3'])
-    %         end
-    
-    
-    
-    
-    %     elseif RLSfile==1 %if input is rls struct
-    if isfield(rlsfile(1),'divSignal')
-        for cond=1:numel(condition)
-            rlstmp=rlsfile([rlsfile.condition]==condition(cond));
-            obj{cond}(1,1)=rlstmp(1).divSignal;
-            
-            listfields=fieldnames(obj{cond}(1,1));
-            for r=rois{cond}
-                for f=1:numel(listfields)
-                    if isfield(rlstmp(r).divSignal,listfields{f})
-                        obj{cond}(r,1).(listfields{f})=rlstmp(r).divSignal.(listfields{f}); %assign obj
-                    end
-                end
-            end
-        end
-    else
-        error(['The roi ' rlsfile(1).name 'has no divSignal field. Extract it using extractSignal followed by measureRLS2'])
-    end
+elseif timeOrGen==1 %generations  
     
     if isfield(rlsfile(1),'Aligned')
         askPlotAligned=input('Aligned signals available, plot this? y/n (Default: n)','s');
-        if numel(askPlotAligned)==0
+        if numel(askPlotAligned)==0 || strcmp(askPlotAligned,'n')
             plotAligned=0;
         elseif strcmp(askPlotAligned,'y') %plot aligned signals
             plotAligned=1;
-            clear obj
-            %remove nosep
-            flag=[];
-            for r=1:numel(rlsfile)
-                if isempty(rlsfile(r).Aligned)
-                    flag=[flag r];
-                end
-            end
-            rlsfile(flag)=[];
-            for cond=1:numel(condition) %updates variable storing number of rois
-                rois{cond}=1:numel(rlsfile([rlsfile.condition]==condition(cond)));
-            end
-            
-            liststrid=fields(rlsfile(1).Aligned); %full, cell, nucleus
-            str=[];
-            for i=1:numel(liststrid)
-                str=[str num2str(i) ' - ' liststrid{i} '; '];
-            end
-            alignid=input(['Which alignment used ? (Default: 1)' str]);
-            if numel(alignid)==0
-                alignid=1;
-            end
-            alignstrid=liststrid{alignid};
-            
+        end
+    end
+    
+    if plotAligned==0
+        if isfield(rlsfile(1),'divSignal')
             for cond=1:numel(condition)
                 rlstmp=rlsfile([rlsfile.condition]==condition(cond));
+                obj{cond}(1,1)=rlstmp(1).divSignal;
                 
-                listfields=fieldnames(obj{cond}(1,1));                            
+                listfields=fieldnames(obj{cond}(1,1));
                 for r=rois{cond}
                     for f=1:numel(listfields)
-                        if isfield(rlstmp(r).Aligned.(alignstrid),listfields{f})
-                            obj{cond}(r,1)=rlstmp(r).Aligned.(alignstrid).(listfields{f}); %assign obj
+                        if isfield(rlstmp(r).divSignal,listfields{f})
+                            obj{cond}(r,1).(listfields{f})=rlstmp(r).divSignal.(listfields{f}); %assign obj
                         end
                     end
                 end
             end
+        else
+            error(['The roi ' rlsfile(1).name 'has no divSignal field. Extract it using extractSignal followed by measureRLS3'])
+        end
+        
+    elseif plotAligned==1
+        %remove nosep
+        flag=[];
+        for r=1:numel(rlsfile)
+            if isempty(rlsfile(r).Aligned)
+                flag=[flag r];
+            end
+        end
+        rlsfile(flag)=[];
+        for cond=1:numel(condition) %updates variable storing number of rois
+            rois{cond}=1:numel(rlsfile([rlsfile.condition]==condition(cond)));
+        end
+        
+        liststrid=fields(rlsfile(1).Aligned); %full, cell, nucleus
+        str=[];
+        for i=1:numel(liststrid)
+            str=[str num2str(i) ' - ' liststrid{i} '; '];
+        end
+        alignid=input(['Which alignment used ? (Default: 1)' str]);
+        if numel(alignid)==0
+            alignid=1;
+        end
+        alignstrid=liststrid{alignid};
+        
+        for cond=1:numel(condition)
+            rlstmp=rlsfile([rlsfile.condition]==condition(cond));
             
+            listfields=fieldnames(obj{cond}(1,1));
+            for r=rois{cond}
+                for f=1:numel(listfields)
+                    if isfield(rlstmp(r).Aligned.(alignstrid),listfields{f})
+                        obj{cond}(r,1)=rlstmp(r).Aligned.(alignstrid).(listfields{f}); %assign obj
+                    end
+                end
+            end
         end
     end
-    %     end
     
-    %     %ask if want to plot divDuration
-    %     if isfield(obj,'divDuration')
-    %         askPlotDivDuration=input('Division duration available, plot this? y/n (Default: n)','s');
-    %         if numel(askPlotDivDuration)==0
-    %             plotDivDur=0;
-    %         elseif strcmp(askPlotDivDuration,'y') %plot aligned signals
-    %             plotDivDur=1;
-    %         end
-    %     end
 end
-
+end
 
 %% ask signal
 liststrid=fields(obj{1}(1)); %full, cell, nucleus, div
