e12b699742bc2340c24d89f876bd3ce5fa6494a3
Gilles Charvin <gilles.charvin@gmail.com>
2022-07-28 16:53:37 +0200

    bug fix classi bk and restore

--- @classi/bk.m
+++ @classi/bk.m
@@ -18,11 +18,12 @@ function bk(classif)
             save(fullfile(pth,[file '_' str '.mat']),'classifier');
             save(fullfile(pth,[file '_classification_' str '.mat']),'classiObj');
             
-            src=    fullfile(pth,'netCNN.mat');
-            if exist(src)
-                 target=fullfile(pth,['netCNN_' str '.mat']);
-                 copyfile(src,target);  
-            end
+%             src=    fullfile(pth,'netCNN.mat');
+%             load(src)
+%             if exist(src)
+%                  target=fullfile(pth,['netCNN_' str '.mat'])
+%                  copyfile(src,target);  
+%             end
            
             src=    fullfile(pth,['netCNN_' classif.strid '.mat']);
             if exist(src)
@@ -30,11 +31,11 @@ function bk(classif)
                  copyfile(src,target);  
             end
             
-            src=    fullfile(pth,'netLSTM.mat');
-             if exist(src)
-                 target=fullfile(pth,['netLSTM_' str '.mat']);
-                 copyfile(src,target);  
-             end
+%             src=    fullfile(pth,'netLSTM.mat');
+%              if exist(src)
+%                  target=fullfile(pth,['netLSTM_' str '.mat']);
+%                  copyfile(src,target);  
+%              end
             
                src=    fullfile(pth,['netLSTM_' classif.strid '.mat']);
              if exist(src)
--- @classi/restore.m
+++ @classi/restore.m
@@ -14,31 +14,47 @@ function restore(classif,index)
   evalin('base', ['clear ' classif.strid]); % clear classifier variable if loaded.
   
   ff=fullfile(pth,t{index,1});
-   copyfile(ff,fullfile(pth,[classif.strid '_classification.mat']));
-  load(ff); % classiObj;
+
+  [classiObj msg]=classiLoad(ff);
+
+ %  copyfile(ff,fullfile(pth,[classif.strid '_classification.mat']));
+  %load(ff); % classiObj;
   
 m=fieldnames(classif);
 
 for i=1:numel(m)
+    if ~strcmp(m{i},'roi') % preserve ROI info which cannot be preserved and bakckup
     classif.(m{i})=classiObj.(m{i});
+    end
 end
+
+  save([classif.strid '_classification.mat'],'classiObj');
+
  
   ff=fullfile(pth,t{index,4});
- copyfile(ff,fullfile(pth,[classif.strid '.mat']));
- 
+ % exist(ff)
+  targ=fullfile(pth,[classif.strid '.mat']);
+
+ load(ff); % loads classifier
+ save(targ,'classifier');
+% copyfile(ff,fullfile(pth,[classif.strid '.mat']));
  
     src=fullfile(pth,['netCNN_' t{index,4}]);
     target=fullfile(pth,['netCNN_' classif.strid '.mat']);
     
      if exist(src)
-                 copyfile(src,target);  
+             load(src); % loads classifier
+             save(target,'classifier')
+              %   copyfile(src,target);  
      end
      
      src=fullfile(pth,['netLSTM_' t{index,4}]);
     target=fullfile(pth,['netLSTM_' classif.strid '.mat']);
     
      if exist(src)
-                 copyfile(src,target);  
+          load(src); % loads classifier
+          save(target,'netLSTM','info')
+             %    copyfile(src,target);  
      end
          
             
