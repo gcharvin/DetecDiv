cce23329ff87327de0f6287667364dda45412a29
Gilles Charvin <gilles.charvin@gmail.com>
2024-01-22 11:30:40 +0100

    update roi extraction

--- @shallow/saveCroppedImages.m
+++ @shallow/saveCroppedImages.m
@@ -554,7 +554,9 @@ for i=fovid
 
             if init==1
              %   tmproi(l).image=uint16(zeros(rroi(4),rroi(3),numel(tmpfov(i).channel),numel(nframestot)));
-                 tmproi(l).image=uint16(zeros(rroi(4),rroi(3),ccha,numel(nframestot)));
+         
+                 tmproi(l).image=uint16(zeros(scale*rroi(4),scale*rroi(3),ccha,numel(nframestot)));
+         
              %   ccha
                 tmproi(l).display.channel={};
                 tmproi(l).display.frame=1;
@@ -568,7 +570,12 @@ for i=fovid
                 cumck=1;
 
                 for k=cha
+                    if  numel(tmpfov(i).channel{k})==0
+                     tmproi(l).display.channel{ck}=['Channel_' num2str(k)];
+                    else
                     tmproi(l).display.channel{ck}=tmpfov(i).channel{k}; %['Channel ' num2str(k)];
+                    end
+
                     if arrcha(ck)==1
                     tmproi(l).display.intensity(ck,:)=temp;
                     tmproi(l).channelid(ck)=ck;
@@ -645,7 +652,12 @@ for i=fovid
             rroitmp(3)=min(rroi(1)+rroi(3)-1,size(list,2));
             rroitmp(4)=min(rroi(2)+rroi(4)-1,size(list,1));
 
-            tmproi(l).image(:,:,:,nframes)=list(rroitmp(2):rroitmp(4),rroitmp(1):rroitmp(3),:,:);
+            tmpfinal=list(rroitmp(2):rroitmp(4),rroitmp(1):rroitmp(3),:,:);
+            if scale~=1
+                tmpfinal=imresize(tmpfinal,scale);
+            end
+
+            tmproi(l).image(:,:,:,nframes)=tmpfinal;
 
             try
                 tmproi(l).save;
