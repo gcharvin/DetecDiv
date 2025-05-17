443d1656e8f95f4727b3510c792dc0d1742e00ea
Gilles Charvin <gilles.charvin@gmail.com>
2025-05-17 15:46:32 +0200

    Update shallowLoad.m

--- shallowLoad.m
+++ shallowLoad.m
@@ -1,167 +1,122 @@
-function [shallowObj, msg]=shallowLoad(filename)
+function [shallowObj, msg] = shallowLoad(filename)
 
-
-if nargin==0
-   [file,path] = uigetfile('*.mat','Select a shallow project',pwd);
-   if isequal(file,0)
-   disp('User selected Cancel')
-   msg=[];
-   shallowObj=[];
-   return;
-   else
-   disp(['User selected ', fullfile(path, file)]); 
-   filename=fullfile(path, file);
-   end
+if nargin == 0
+    [file, path] = uigetfile('*.mat', 'Select a shallow project', pwd);
+    if isequal(file, 0)
+        disp('User selected Cancel');
+        msg = [];
+        shallowObj = [];
+        return;
+    else
+        disp(['User selected ', fullfile(path, file)]); 
+        filename = fullfile(path, file);
+    end
 end
 
-[path file ext]=fileparts(filename);
-
-%filename
-abspath=what(path);
-abspath=abspath.path;
-
-filename=fullfile(abspath,[file ext]);
-
+[path, file, ext] = fileparts(filename);
+abspath = what(path);
+abspath = abspath.path;
+filename = fullfile(abspath, [file ext]);
 load(filename);
 
-if ~exist('shallowObj')
-disp('this is not a shallow object ! Quitting....');
-msg=['Wrong file name loaded'];
-  shallowObj=[];
-return;
+if ~exist('shallowObj', 'var')
+    disp('this is not a shallow object ! Quitting....');
+    msg = ['Wrong file name loaded'];
+    shallowObj = [];
+    return;
 end
 
-if ~isfield(shallowObj.processing,'processor')
-    shallowObj.processing.processor=[];
+if ~isfield(shallowObj.processing, 'processor')
+    shallowObj.processing.processor = [];
 end
-
-path=abspath;
 
 if isunix || ismac
-shallowObj.setPath([path '/'],file); % adjust path
+    shallowObj.setPath([path '/'], file);
 else
-shallowObj.setPath([path '\'],file); % adjust path 
+    shallowObj.setPath([path '\'], file);
 end
 
-% check if project is already open in the workspace
-varlist=evalin('base','who');
-     for i=1:numel(varlist)
-                
-                if strcmp(varlist{i},'ans')
-                        continue;
-                end
-                
-                 tmp=evalin('base',varlist{i});
-                 if isa(tmp,'shallow')
-                     % check path & filenemae
-                     if strcmp(path, tmp.io.path(1:end-1)) & strcmp(file, tmp.io.file) % var exists already
-                         msg=['Project is already in the workspace under the var name: ' varlist{i} '; Quitting...'];
-                         disp(msg);
-                         shallowObj=[];
-                         return
-                     end
-                 end
-     end
+% Vérification si projet déjà chargé
+varlist = evalin('base', 'who');
+for i = 1:numel(varlist)
+    if strcmp(varlist{i}, 'ans')
+        continue;
+    end
+    tmp = evalin('base', varlist{i});
+    if isa(tmp, 'shallow')
+        if strcmp(path, tmp.io.path(1:end-1)) && strcmp(file, tmp.io.file)
+            msg = ['Project is already in the workspace under the var name: ' varlist{i} '; Quitting...'];
+            disp(msg);
+            shallowObj = [];
+            return;
+        end
+    end
+end
 
-msg=['Successfully loaded shallow project ' fullfile(path,[file '.mat']) '!'];
+msg = ['Successfully loaded shallow project ' fullfile(path, [file '.mat']) '!'];
 disp(msg);
 disp('');
 
-% now loading saved classi objects attached to project 
+%% Chargement des classifieurs
 
-% here browse the classification folder and load all avaiable classifiers 
+listclassi = dir(fullfile(path, file, 'classification'));
+listclassi = listclassi(~contains({listclassi.name}, {'.', '..'}));
+listclassi = listclassi(arrayfun(@(x) x.isdir, listclassi));
 
-listclassi=dir(fullfile(path,file,'classification'));
-listclassi=listclassi(~contains({listclassi.name},{'.','..'}));
-listclassi=listclassi(find(arrayfun(@(x) x.isdir==1,listclassi)));
-
-% sort the classi by ending number
-
-if numel(listclassi)
- shallowObj.processing.classification=classi;
-end
- 
-arr=[];
-for j=1:numel(listclassi)
-tmp=regexp(listclassi(j).name, '\d+$','match') ;
-arr(j)=str2num(tmp{1});
-end
-
-[s ix]=sort(arr);
-
-listclassi=listclassi(ix);
-
-for j=1:numel(listclassi)
-    
-    name=listclassi(j).name;
-    str=fullfile(path,file,'classification',name,[name '_classification.mat']);
-    
-    if exist(str)
-    [classiObj msg]=classiLoad(str);
-    
-     shallowObj.processing.classification(j)=classiObj;
+if ~isempty(listclassi)
+    arr = zeros(1, numel(listclassi));
+    for j = 1:numel(listclassi)
+        tmp = regexp(listclassi(j).name, '\d+$', 'match');
+        arr(j) = str2double(tmp{1});
     end
-    
-end
+    [~, ix] = sort(arr);
+    listclassi = listclassi(ix);
 
-
-
-
-% now loading saved processor objects attached to project 
-
-% here browse the classification folder and load all avaiable classifiers 
-
-listclassi=dir(fullfile(path,file,'processor'));
-listclassi=listclassi(~contains({listclassi.name},{'.','..'}));
-listclassi=listclassi(find(arrayfun(@(x) x.isdir==1,listclassi)));
-% sort the classi by ending number
-
-if numel(listclassi)
- shallowObj.processing.processor=process;
-end
- 
-arr=[];
-for j=1:numel(listclassi)
-tmp=regexp(listclassi(j).name, '\d+$','match') ;
-arr(j)=str2num(tmp{1});
-end
-
-[s ix]=sort(arr);
-
-listclassi=listclassi(ix);
-
-for j=1:numel(listclassi)
-    
-    name=listclassi(j).name;
-    str=fullfile(path,file,'processor',name,[name '_processor.mat']);
-    
-    if exist(str)
-    [classiObj msg]=processLoad(str);
+    for j = 1:numel(listclassi)
+        name = listclassi(j).name;
+        str = fullfile(path, file, 'classification', name, [name '_classification.mat']);
+        if exist(str, 'file') == 2
+            [classiObj, msgclassi] = classiLoad(str);
+            shallowObj.processing.classification(j) = classiObj;
+        end
     end
-    
-     shallowObj.processing.processor(j)=classiObj;
 end
 
+%% Chargement des processeurs
 
+listproc = dir(fullfile(path, file, 'processor'));
+listproc = listproc(~contains({listproc.name}, {'.', '..'}));
+listproc = listproc(arrayfun(@(x) x.isdir, listproc));
 
-% for i=1:numel(shallowObj.processing.classification)
-%     [pathc,filec]=shallowObj.processing.classification(i).getPath;
-%     
-%  [classiObj msg]=classiLoad(fullfile(pathc,[filec '_classification.mat']));
-%  
-% 
-% end
+if ~isempty(listproc)
+    arr = zeros(1, numel(listproc));
+    for j = 1:numel(listproc)
+        tmp = regexp(listproc(j).name, '\d+$', 'match');
+        arr(j) = str2double(tmp{1});
+    end
+    [~, ix] = sort(arr);
+    listproc = listproc(ix);
 
-if numel(shallowObj.fov(1).srcpath{1})~=0 % srce path has been set for at leats one FOV; update ? 
-disp('* Warning *');
-disp('* This project contains at least one FOV wit the following path');
-disp('* The current path are:*');
-for i=1:numel(shallowObj.fov)
-disp([shallowObj.fov(i).srcpath{1}]);
-% new ref to parent
-shallowObj.fov(i).parent=shallowObj;
+    for j = 1:numel(listproc)
+        name = listproc(j).name;
+        str = fullfile(path, file, 'processor', name, [name '_processor.mat']);
+        if exist(str, 'file') == 2
+            [procObj, msgproc] = processLoad(str);
+            shallowObj.processing.processor(j) = procObj;
+        end
+    end
 end
-disp('* Need to update the path of the source images ?');
-disp('* To do so, use the shallowObj.setSrcPath function');
+
+%% Vérification des FOV
+if numel(shallowObj.fov(1).srcpath{1}) ~= 0
+    disp('* Warning *');
+    disp('* This project contains at least one FOV with the following path:');
+    for i = 1:numel(shallowObj.fov)
+        disp(shallowObj.fov(i).srcpath{1});
+        shallowObj.fov(i).parent = shallowObj;
+    end
+    disp('* Need to update the path of the source images ?');
+    disp('* To do so, use the shallowObj.setSrcPath function');
 end
 
+end
