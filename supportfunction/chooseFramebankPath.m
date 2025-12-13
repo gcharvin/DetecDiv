6270b11f0f88841500935f2e2c0560368d68d93b
Gilles Charvin <gilles.charvin@gmail.com>
2025-12-13 08:43:55 +0100

    bug fix

--- supportfunction/chooseFramebankPath.m
+++ supportfunction/chooseFramebankPath.m
@@ -1,4 +1,4 @@
-function fbPath info = chooseFramebankPath(basePath)
+function fbPath = chooseFramebankPath(basePath)
         % Choisit un chemin de framebank "sain" :
         % - teste basePath, puis basePath_001, basePath_002, ...
         % - si un chemin existe et est supprimable -> on le réutilise
