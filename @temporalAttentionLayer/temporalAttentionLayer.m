f13bae97d50e823582741280c7fe04ac3fe34362
Gilles Charvin <gilles.charvin@gmail.com>
2025-12-15 11:48:43 +0100

    Update temporalAttentionLayer.m

--- @temporalAttentionLayer/temporalAttentionLayer.m
+++ @temporalAttentionLayer/temporalAttentionLayer.m
@@ -18,27 +18,81 @@ classdef temporalAttentionLayer < nnet.layer.Layer ...
             layer.b = single(0);
         end
 
-     function Z = predict(layer, X)
-    % X : [H x T x B] (TCB, dlarray formaté)
+function Z = predict(layer, X)
+    % Accept X as:
+    %  - [F x T] 
+    %  - [F x T x B]
+    %  - [F x 1 x T x B] (possible after some sequence plumbing)
+    %
+    % We apply a scalar gate per time step (and per batch item).
 
-    % --- enlever temporairement les labels ---
-    Xd = stripdims(X);   % devient dlarray non formaté
+    Xd = stripdims(X);
+    sz = size(Xd);
+    nd = ndims(Xd);
 
-    % --- poids ---
-    W = reshape(layer.W, [], 1, 1);   % [H x 1 x 1]
+    % --- Normalize to [F x T x B] ---
+    if nd == 2
+        % [F x T] -> [F x T x 1]
+        F = sz(1); T = sz(2); B = 1;
+        Xn = reshape(Xd, [F T B]);
 
-    % --- score linéaire par frame ---
-    s = sum(Xd .* W, 1) + layer.b;    % [1 x T x B]
+    elseif nd == 3
+        % [F x T x B]
+        F = sz(1); T = sz(2); B = sz(3);
+        Xn = Xd;
 
-    % --- gate sigmoid ---
-    g = 1 ./ (1 + exp(-s));           % [1 x T x B]
+    elseif nd == 4
+        % Common case: [F x 1 x T x B] or [F x T x 1 x B]
+        % We try to detect where T and B are.
+        F = sz(1);
 
-    % --- gating résiduel ---
-    Zd = Xd .* (1 + g);               % [H x T x B]
+        % If second dim is singleton, assume [F x 1 x T x B]
+        if sz(2) == 1
+            T = sz(3); B = sz(4);
+            Xn = reshape(Xd, [F T B]);
+        % If third dim is singleton, assume [F x T x 1 x B]
+        elseif sz(3) == 1
+            T = sz(2); B = sz(4);
+            Xn = reshape(Xd, [F T B]);
+        else
+            error('temporalAttentionLayer:UnsupportedShape', ...
+                'Unsupported 4D shape: %s', mat2str(sz));
+        end
+    else
+        error('temporalAttentionLayer:UnsupportedNdims', ...
+            'Unsupported ndims=%d shape=%s', nd, mat2str(sz));
+    end
 
-    % --- remettre les labels d'origine ---
-    Z = dlarray(Zd, dims(X));         % restaure "TCB"
+    % --- Check feature dimension matches W ---
+    W = layer.W;  % [F x 1] expected
+    if size(W,1) ~= size(Xn,1)
+        error('temporalAttentionLayer:FeatureMismatch', ...
+            'Feature mismatch: size(X,1)=%d but size(W,1)=%d', size(Xn,1), size(W,1));
+    end
+
+    Wb = reshape(W, [], 1, 1);            % [F x 1 x 1]
+    s  = sum(Xn .* Wb, 1) + layer.b;      % [1 x T x B]
+    g  = 1 ./ (1 + exp(-s));              % [1 x T x B]
+    Yn = Xn .* (1 + g);                   % [F x T x B]
+
+    % --- Restore original shape ---
+    if nd == 2
+        Zd = reshape(Yn, [F T]);
+    elseif nd == 3
+        Zd = Yn;
+    else % nd==4
+        % return to the same 4D layout as input
+        if sz(2) == 1
+            Zd = reshape(Yn, [F 1 T B]);
+        else
+            Zd = reshape(Yn, [F T 1 B]);
+        end
+    end
+
+    % restore labels if they existed
+    Z = dlarray(Zd, dims(X));
 end
+
 
 
     end
