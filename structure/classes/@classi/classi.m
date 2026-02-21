dced9be770ee6eb418b78c74512b9ffce9e5aa5d
Gilles Charvin <gilles.charvin@gmail.com>
2026-02-21 09:05:39 +0100

    updated bounds in cnn/lstm

--- structure/classes/@classi/classi.m
+++ structure/classes/@classi/classi.m
@@ -19,7 +19,7 @@ classdef classi < handle
         trainingFun='';
         classifierPkg=''; % package folder name (e.g. "cnn_lstm") for standardized dispatch
         colormap=[];
-        bounds= struct('Type','Auto','Rules',struct('Dataseries',{[]},'Dataset',{[]},'Value',{[]},'Occurence',[0],'Offset',[0 ])); % type can be : auto,  manual, rules;   'Rules' is a struc that specifies the type of rules : ; 'Values' specifies the automated interval set for all ROIs
+        bounds= struct('Type','Auto','Values',[],'Rules',struct('Dataseries',{[]},'Dataset',{[]},'Value',{[]},'Occurence',[0],'Offset',[0 ])); % type can be : auto,  manual, rules;   'Rules' is a struc that specifies the type of rules : ; 'Values' specifies the automated interval set for all ROIs
 
         score=[]; %struct('roisid',[],'recall',[],'accuracy',[],'fscore',[],'confusion',[],'classes',[],'rois',[]); %  a structure that stores the scores of the classification , which is done by the stats method
 
